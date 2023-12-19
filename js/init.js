ad8432acba7c22bdbb1c1e0eb8a2fc17ae86e7dc
chat43slda <153542696+chat43slda@users.noreply.github.com>
2023-12-18 20:26:14 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+  apiKey: "AIzaSyAS2sCX6lbGjlpbFP6C422wYrr0F_ZzeAY",
+  authDomain: "bdfalumnos-4b015.firebaseapp.com",
+  projectId: "bdfalumnos-4b015",
+  storageBucket: "bdfalumnos-4b015.appspot.com",
+  messagingSenderId: "574866533002",
+  appId: "1:574866533002:web:1abc69caa7f58460ccdb73",
+  measurementId: "G-2HQ934D5W2"
 });
